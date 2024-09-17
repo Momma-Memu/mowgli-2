d5a62151ed9d764175bd55a33e7d7cf94e1f9ef3
Miah Barnes <miahellenbarnes@gmail.com>
2024-09-17 06:54:22 -0400

    sessions working finally

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,12 +15,33 @@ export default defineConfig({
         }
       }
     }),
-    vueJsx()
+    vueJsx(),
+    basicSsl({
+      name: "test",
+      // domains: [""],
+      // certDir: ""
+    }),
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
+  },
+  server: {
+    // https: {
+    //   cert: "./certs/cert.pem",
+    //   key: "./certs/key.pem",
+    // },
+    proxy: "https://localhost:3000"
+    // proxy: {
+    //   "/api": {
+    //     target: "https://638a4d4f66a0.ngrok.app",
+    //     changeOrigin: true,
+    //     secure: true, 
+    //     ws: true,
+    //     rewrite: path => path.replace(/^\/api/, ''),
+    //   }
+    // }
   },
   assetsInclude: ["**/*.html", "**/*.svg"]
 });
